13452a5cea338d207a7f81c6054ae36e14895497
Isaac <izaroot327@gmail.com>
2021-04-09 15:34:42 -0400

    Submit is working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  let taskList = document.querySelector("#tasks")
+  let taskForm = document.querySelector("#create-task-form")
+  taskForm.addEventListener("submit", function(evt){
+    evt.preventDefault()
+
+    let formBox = evt.target
+    let inputBox = formBox.tomato
+    let taskValue = inputBox.value
+    
+    let blankLi = document.createElement("li")
+    blankLi.className = "task-item"
+    blankLi.innerText = taskValue
+    
+    taskList.append(blankLi)
+    console.log("hi")
+  })
+// });
